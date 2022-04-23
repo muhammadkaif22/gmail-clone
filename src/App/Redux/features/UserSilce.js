60c3214230b2f947cf0ffdaa2a30419239e80f44
Xcoderx <xcoder304@gmail.com>
2022-04-23 14:08:12 +0500

    just starting the app

---
+++ src/App/Redux/features/UserSilce.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.user = action.payload;
+    },
+    loginOut: (state, action) => {
+      state.user = null;
+    },
+  },
+});
+
+export const { signin, signOut } = userSlice.actions;
+
+export const selectUser = (state) => state.user.user;
+
+export default userSlice.reducer;
